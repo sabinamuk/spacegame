744fdadf221be50b101e3c1b71d8c984a027aca9
legewae <55580266+legewae@users.noreply.github.com>
2026-05-23 13:57:55 +0600

    Fix formatting

--- SpaceBattle.Tests/ICommandReceiver_tests.cs
+++ SpaceBattle.Tests/ICommandReceiver_tests.cs
@@ -2,7 +2,6 @@
 using SpaceBattle.Lib;
 using Xunit.Sdk;
 namespace SpaceBattle.Tests;
-
 public class IMessageReceiver_tests
 {
     [Fact]
@@ -11,15 +10,13 @@ public class IMessageReceiver_tests
         //Arrange
         var mockCommand = new Mock<ICommand>();
         var mockReceiver = new Mock<IMessageReceiver>();
-        var sendCommand = new SendCommand(mockCommand.Object, mockReceiver.Object); 
+        var sendCommand = new SendCommand(mockCommand.Object, mockReceiver.Object);
 
         //Act
         sendCommand.Execute();
-
         //Assert
         mockReceiver.Verify(r => r.Receive(mockCommand.Object), Times.Once); //Проверяем историю вызовов, что метод Receive был вызван один раз, а в аргументе правильная команда
     }
-
     class Faulty_Receiver : IMessageReceiver
     {
         public void Receive(ICommand command)
@@ -35,7 +32,6 @@ public class IMessageReceiver_tests
         var bad_receiver = new Faulty_Receiver();
         var mockCommand = new Mock<ICommand>();
         var sendCommand = new SendCommand(mockCommand.Object, bad_receiver);
-        
         bool error_returned = false;
         //Act
         try
@@ -46,9 +42,7 @@ public class IMessageReceiver_tests
         {
             error_returned = true;
         }
-
         //Assert
         Assert.True(error_returned, "Ожидали получить InvalidOperationException при вызове Execute у SendCommand с IMessageReceiver, всегда возвращающим исключение");
     }
 }
-
