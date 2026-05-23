af87d8d4fbed6cf0cc1c2a6eab5d95cbb9d21a61
Lena <ulidka12345@yandex.ru>
2026-05-23 15:55:57 +0600

    add macrocommandstrategy changed

--- SpaceBattle.Lib/MacroCommandStrategy.cs
+++ SpaceBattle.Lib/MacroCommandStrategy.cs
@@ -16,16 +16,35 @@ public class CreateMacroCommandStrategy
 
     public ICommand Resolve(object[] args)
     {
+        var emptyArgs = new Dictionary<string, object>();
+
         var commandNames =
             (IEnumerable<string>)
-            Ioc.Resolve("Specs." + commandSpec);
+            Ioc.Resolve("Specs." + commandSpec, emptyArgs);
+
+        var dictArgs = argsToDict(args);
 
         var commands =
             commandNames
                 .Select(name =>
-                    (ICommand)Ioc.Resolve(name, args))
+                    (ICommand)Ioc.Resolve(name, dictArgs))
                 .ToList();
 
         return new MacroCommand(commands);
+    }
+
+    private Dictionary<string, object> argsToDict(object[] args)
+    {
+        var dict = new Dictionary<string, object>();
+
+        if (args == null || args.Length == 0)
+            return dict;
+
+        for (int i = 0; i < args.Length - 1; i += 2)
+        {
+            dict[(string)args[i]] = args[i + 1];
+        }
+
+        return dict;
     }
 }
