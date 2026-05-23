e2b3f460c5607b0bd2ce6bba80c6436ca5a87216
Lena <ulidka12345@yandex.ru>
2026-05-23 14:11:44 +0600

    add rotatecommand and tests

---
+++ SpaceBattle.Lib/RotateCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Windows.Input;
+
+namespace SpaceBattle.Lib;
+
+
+public interface IRotatingObject
+{
+    Angle Angle { get; set; }
+
+    Angle AngularVelocity { get; }
+}
+
+public class RotateCommand : ICommand
+{
+    private readonly IRotatingObject obj;
+
+    public RotateCommand(IRotatingObject obj)
+    {
+        this.obj = obj;
+    }
+
+    public void Execute()
+    {
+        obj.Angle = obj.Angle + obj.AngularVelocity;
+    }
+}
