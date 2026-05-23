25e541a655e1b2fca1786dec488a1322395bf0c7
legewae <55580266+legewae@users.noreply.github.com>
2026-05-23 13:27:08 +0600

    Add SendCommand and IMessageReceiver with tests

---
+++ SpaceBattle.Lib/ICommandReceiver.cs
@@ -0,0 +1,8 @@
+﻿
+namespace SpaceBattle.Lib;
+
+public interface IMessageReceiver //Интерфейс для получения команды, которая будет выполняться в другом месте(Может реализовывать класс очереди для команд, например)
+{
+    void Receive(ICommand command);
+}
+
---
+++ SpaceBattle.Lib/SendCommand.cs
@@ -0,0 +1,21 @@
+﻿
+namespace SpaceBattle.Lib
+{
+    public class SendCommand : ICommand
+    {
+        private readonly ICommand _command;
+        private readonly IMessageReceiver _receiver;
+
+        public SendCommand(ICommand command, IMessageReceiver receiver)
+        {
+            _command = command;
+            _receiver = receiver;
+        }
+
+        public void Execute()
+        {
+            _receiver.Receive(_command);
+        }
+    }
+}
+
