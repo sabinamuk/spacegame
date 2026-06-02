03083e3c3c8954db0f1301ed87edb52d1da3551c
legewae <55580266+legewae@users.noreply.github.com>
2026-06-02 12:48:28 +0600

    implement CommandInjectableCommand + tests

---
+++ SpaceBattle.Tests/CommandInjectableCommand_test.cs
@@ -0,0 +1,33 @@
+using Moq;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class CommandInjectableCommand_test
+{
+    [Fact]
+    public void Execute_CallsInjectedCommand_WhenCommandIsInjected()
+    {
+        // Arrange
+        var commandMock = new Mock<ICommand>();
+        var sut = new CommandInjectableCommand();
+
+        // Act
+        sut.Inject(commandMock.Object);
+
+        sut.Execute();
+        
+        // Assert
+        commandMock.Verify(c => c.Execute(), Times.Once);
+    }
+
+    [Fact]
+    public void Execute_ThrowsException_WhenNoCommandInjected()
+    {
+        // Arrange
+        var sut = new CommandInjectableCommand();
+
+        // Act + Assert
+        Assert.Throws<Exception>(() => sut.Execute());
+    }
+}
