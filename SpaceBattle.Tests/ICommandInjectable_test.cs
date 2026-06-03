ada27e3104d899a625a14c00f6d69e2caa87826e
legewae <55580266+legewae@users.noreply.github.com>
2026-06-03 12:26:13 +0600

    ICommandInjectable test

---
+++ SpaceBattle.Tests/ICommandInjectable_test.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class ICommandInjectable_test
+{
+    [Fact]
+    public void Inject_CallsInjectWithCorrectCommand()
+    {
+        var command = new Mock<ICommand>();
+        var injectable = new Mock<ICommandInjectable>();
+
+        injectable.Object.Inject(command.Object);
+
+        injectable.Verify(i => i.Inject(command.Object), Times.Once);
+    }
+}
