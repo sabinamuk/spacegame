5643ef92156aa909b9ba0575b12a538a635d1cae
legewae <55580266+legewae@users.noreply.github.com>
2026-06-03 12:07:14 +0600

    fix formatting

--- SpaceBattle.Lib/RegisterIoCDependencySendCommand.cs
+++ SpaceBattle.Lib/RegisterIoCDependencySendCommand.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SpaceBattle.Lib {
+namespace SpaceBattle.Lib
+{
     public class RegisterIoCDependencySendCommand : ICommand
-    {   
+    {
         public void Execute()
         {
             Ioc.Register("Commands.Send", args => new SendCommand((ICommand)args["command"], (ICommandReceiver)args["receiver"]));
