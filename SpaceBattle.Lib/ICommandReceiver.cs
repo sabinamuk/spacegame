896b62c6a481d92903621d1ddcdc087ed9f5d638
legewae <55580266+legewae@users.noreply.github.com>
2026-06-06 13:41:18 +0600

    implement

---
+++ SpaceBattle.Lib/ICommandReceiver.cs
@@ -0,0 +1,7 @@
+﻿
+namespace SpaceBattle.Lib;
+
+public interface ICommandReceiver //Интерфейс для получения команды, которая будет выполняться в другом месте(Может реализовывать класс очереди для команд, например)
+{
+    void Receive(ICommand command);
+}
