0bc3b7a0dcd3c3676cdd73ae886b86ac38d4fdf0
legewae <55580266+legewae@users.noreply.github.com>
2026-06-06 14:43:27 +0600

    fafa

--- SpaceBattle.Tests/MacroMoveRotateTests.cs
+++ SpaceBattle.Tests/MacroMoveRotateTests.cs
@@ -9,34 +9,6 @@ public class MacroMoveRotateTests
 {
 
     [Fact]
-    public void MacroRotate_Executes_All_Commands()
-    {
-        var cmd1 = new Mock<ICommand>();
-        var cmd2 = new Mock<ICommand>();
-
-        Ioc.Register("Commands.A", _ => cmd1.Object);
-        Ioc.Register("Commands.B", _ => cmd2.Object);
-
-        Ioc.Register("Specs.Rotate", _ =>
-            new List<string>
-            {
-                "Commands.A",
-                "Commands.B"
-            });
-
-        new RegisterIoCDependencyMacroMoveRotate().Execute();
-
-        var macro = (ICommand)Ioc.Resolve(
-            "Macro.Rotate",
-            new Dictionary<string, object>());
-
-        macro.Execute();
-
-        cmd1.Verify(c => c.Execute(), Times.Once);
-        cmd2.Verify(c => c.Execute(), Times.Once);
-    }
-
-    [Fact]
     public void MacroMove_Throws_When_Specs_Missing()
     {
         new RegisterIoCDependencyMacroMoveRotate().Execute();
