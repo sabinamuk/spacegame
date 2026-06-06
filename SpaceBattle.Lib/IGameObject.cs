7cf04f08d3b700a76c807eb7fbb4cfda6f870672
Lena <ulidka12345@yandex.ru>
2026-06-06 16:10:30 +0600

    lab-3-repo impl

---
+++ SpaceBattle.Lib/IGameObject.cs
@@ -0,0 +1,6 @@
+namespace SpaceBattle.Lib;
+
+public interface IGameObject
+{
+    string Id { get; }
+}
---
+++ SpaceBattle.Lib/IGameObjectRepository.cs
@@ -0,0 +1,12 @@
+namespace SpaceBattle.Lib;
+
+public interface IGameObjectRepository
+{
+    void Add(string id, IGameObject gameObject);
+
+    IGameObject Get(string id);
+
+    bool Contains(string id);
+
+    int Count();
+}
